07c88dcdf832edf18259e62fa276f3a98491f6e8
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-27 16:02:33 +0600

    count total order

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,6 +3,13 @@ import SingleCard from '../SingleCard/SingleCard';
 
 const Card = () => {
      const [cardData,setCardData] = useState([]);
+     const [orderDetails,setOrderDetails]=useState([])
+
+    const summeryDetails=(orderData)=>{
+        setOrderDetails([...orderDetails,orderData])
+    }
+
+
      useEffect(()=>{
         fetch("products.json")
         .then( res => res.json())
@@ -12,11 +19,12 @@ const Card = () => {
         <div className=' lg:grid grid-cols-5'  >
             <div className='col-span-4 gap-8 mx-[8%]  mt-12 lg:grid grid-cols-3'>
             {
-                cardData.map( singleCard => <SingleCard data={singleCard} ></SingleCard>)
+                cardData.map( singleCard => <SingleCard summery={summeryDetails} data={singleCard} ></SingleCard>)
             }
             </div>
             <div>
-                <h1>Summery</h1>
+                <h1>Order Summay</h1>
+                <h1>Selected Items:{orderDetails.length}</h1>
             </div>
         </div>
         
--- src/Components/SingleCard/SingleCard.jsx
+++ src/Components/SingleCard/SingleCard.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const SingleCard = (props) => {
     console.log(props.data);
     const {img,name,price,seller,ratings}=props.data;
     return (
-            <div className='p-2 rounded-lg border-2' >
-                <img className=' rounded-lg md:h-48 lg:h-72 w-full' src={img} alt="" />
-                <p>{name}</p>
-                <p className='mb-4'>Price:${price}</p>
-                <p>Manufacturer:{seller}</p>
-                <p>Rating: {ratings} stars</p>
+            <div className=' rounded-lg lg:h-full border-2' >
+                <div className=' p-2'>
+                    <img className=' rounded-lg md:h-48 lg:h-full w-full' src={img} alt="" />
+                    <p>{name}</p>
+                    <p className='mb-4'>Price:${price}</p>
+                    <p>Manufacturer:{seller}</p>
+                    <p>Rating: {ratings} stars</p>
+               
+                </div>
+                <hr />
+                 <div className='flex h-[50px] justify-center bg-orange-500  items-center'>
+                    <p onClick={()=>props.summery(props.data)} className='' >Add To Cart <FontAwesomeIcon icon={faCartShopping} /></p>
+                    {/* {faCartShopping} */}
+                </div>
             </div>
     );
 };
