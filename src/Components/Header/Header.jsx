8d9b96e1dc64310b837fbede280bb67a17da5eef
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-27 14:14:43 +0600

    completed the header part

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
     return (
-        <div>
-            <h1>hEADER </h1>
-        </div>
+        <div className='bg-black' >
+            <div className='flex p-4 w-[90%] mx-auto justify-between items-center'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div>
+                <ul className='flex md:gap-1 lg:gap-4 text-white'>
+                    <li>Order</li>
+                    <li>Order Review</li>
+                    <li>Manage Inventory</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+            </div>
+        </div>        
     );
 };
 
