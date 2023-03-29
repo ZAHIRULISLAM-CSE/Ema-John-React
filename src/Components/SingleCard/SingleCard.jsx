af1b9a4b67938b35685aeff10f6cdc038f9a749e
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-30 01:29:42 +0600

    fixed the add to cart button

--- src/Components/SingleCard/SingleCard.jsx
+++ src/Components/SingleCard/SingleCard.jsx
@@ -6,17 +6,17 @@ const SingleCard = (props) => {
     // console.log(props.data);
     const {img,name,price,seller,ratings}=props.data;
     return (
-            <div className=' rounded-lg lg:h-full border-2' >
+            <div className=' flex flex-col justify-between rounded-lg lg:h-full border-2' >
                 <div className=' p-2'>
-                    <img className=' rounded-lg md:h-48 lg:h-full w-full' src={img} alt="" />
+                    <img className=' rounded-lg md:h-48 lg:h-72 w-full' src={img} alt="" />
                     <p>{name}</p>
                     <p className='mb-4'>Price:${price}</p>
                     <p>Manufacturer:{seller}</p>
                     <p>Rating: {ratings} stars</p>
                
                 </div>
-                <hr />
                  <div className='flex h-[50px] justify-center bg-orange-500  items-center'>
+                 <hr />
                     <p onClick={()=>props.summery(props.data)} className='' >Add To Cart <FontAwesomeIcon icon={faCartShopping} /></p>
                     {/* {faCartShopping} */}
                 </div>
