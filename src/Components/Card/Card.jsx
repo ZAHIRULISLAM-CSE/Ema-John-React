7390055ee5b88f6634cb92e8e1b8e8bd5db1a261
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-29 22:14:31 +0600

    setting up the local storage

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,31 +1,51 @@
 import React, { useEffect, useState } from 'react';
+import AddToLocal from '../Database/AddToLocal';
 import SingleCard from '../SingleCard/SingleCard';
 
 const Card = () => {
      const [cardData,setCardData] = useState([]);
-     const [orderDetails,setOrderDetails]=useState([])
+     const [orderDetails,setOrderDetails]=useState([]);
+     const allCartFromLocal=[];
 
     const summeryDetails=(orderData)=>{
-        console.log(orderData);
         setOrderDetails([...orderDetails,orderData]);
+        AddToLocal(orderData.id);
+
     }
-
-    console.log(orderDetails);
-    let totalSum=0;
-    let shippingCharge=0;
-    for(const singleOrderDetails of orderDetails){
-        totalSum=totalSum+singleOrderDetails.price;
-        shippingCharge=shippingCharge+singleOrderDetails.shipping;
-    }
-    let tax=totalSum*7/100;
-    let grandTotal=totalSum+shippingCharge+tax;
-
-
-     useEffect(()=>{
+    useEffect(()=>{
         fetch("products.json")
         .then( res => res.json())
         .then( data => setCardData(data))
      },[])
+
+
+    useEffect(()=>{
+        const savedCart=JSON.parse(localStorage.getItem("shopping-cart"));
+        //get the product with id
+        const storeDataInArray=[];
+        for (const id in savedCart){
+           const savedProduct=cardData.find(product => product.id === id);
+           if(savedProduct){
+                //setting up quantity
+                const quantity=savedCart[id];
+                savedProduct.quantity=quantity;
+                storeDataInArray.push(savedProduct);
+           }
+
+           setOrderDetails(storeDataInArray);
+        }
+        
+    },[cardData])
+
+     let totalSum=0;
+     let shippingCharge=0;
+     for(const singleOrderDetails of orderDetails){
+         totalSum=totalSum+singleOrderDetails.price;
+         shippingCharge=shippingCharge+singleOrderDetails.shipping;
+     }
+     let tax=totalSum*7/100;
+     let grandTotal=totalSum+shippingCharge+tax;
+
     return (
         <div className=' lg:grid grid-cols-5'  >
             <div className='col-span-4 gap-8 mx-[8%]  mt-12 lg:grid grid-cols-3'>
